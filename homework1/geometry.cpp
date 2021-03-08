e8f346ff6f4e64ab1dc883a71fb09ac7d5771d1a
Daninext <47050120+Daninext@users.noreply.github.com>
2021-03-08 22:38:43 +0300

    v2

---
+++ homework1/geometry.cpp
@@ -0,0 +1,165 @@
+#include "geometry.h"
+
+Point::Point(int x, int y)
+	: x_(x), y_(y) 
+{ }
+
+Point::Point(const Point& other)
+	: x_(other.x_), y_(other.y_)
+{ }
+
+int Point::getX() const {
+	return x_;
+}
+
+int Point::getY() const {
+	return y_;
+}
+
+PolygonalChain::PolygonalChain(int n, Point* a) 
+	: PointNum(n)
+{
+	AllPoints = new Point[n];
+	memcpy(AllPoints, a, PointNum * sizeof(Point));
+}
+
+PolygonalChain::PolygonalChain(const PolygonalChain& other)
+	: PointNum(other.PointNum), AllPoints(other.AllPoints)
+{ }
+
+Point PolygonalChain::getPoint(int i) const {
+	return AllPoints[i];
+}
+
+float PolygonalChain::perimeter() const {
+	float perimeter_ = 0;
+	Point* AllVectors = new Point[PointNum - 1];
+	for (int i = 0; i != PointNum - 1; i++) {
+		AllVectors[i] = Point((AllPoints[i + 1].getX() - AllPoints[i].getX()), (AllPoints[i + 1].getY() - AllPoints[i].getY()));
+	}
+	for (int i = 0; i != PointNum - 1; i++) {
+		perimeter_ += (float)sqrt(pow(AllVectors[i].getX(), 2) + pow(AllVectors[i].getY(), 2));
+	}
+
+	return perimeter_;
+}
+
+int PolygonalChain::getN() const {
+	return PointNum;
+}
+
+ClosedPolygonalChain::ClosedPolygonalChain(int n, Point* a)
+	: PolygonalChain(n, a)
+{ }
+
+ClosedPolygonalChain::ClosedPolygonalChain(const ClosedPolygonalChain& other)
+	: PolygonalChain(other) 
+{ }
+
+float ClosedPolygonalChain::perimeter() const {
+	float perimeter_ = 0;
+
+	Point* AllVectors = new Point[PointNum];
+	for (int i = 0; i != PointNum - 1; i++) {
+		AllVectors[i] = Point((AllPoints[i + 1].getX() - AllPoints[i].getX()), (AllPoints[i + 1].getY() - AllPoints[i].getY()));
+	}
+	AllVectors[PointNum - 1] = Point((AllPoints[0].getX() - AllPoints[PointNum - 1].getX()), (AllPoints[0].getY() - AllPoints[PointNum - 1].getY()));
+
+	for (int i = 0; i != PointNum; i++) {
+		perimeter_ += (float)sqrt(pow(AllVectors[i].getX(), 2) + pow(AllVectors[i].getY(), 2));
+	}
+
+	return perimeter_;
+}
+
+Polygon::Polygon(int n, Point* a)
+	: ClosedPolygonalChain(n, a)
+{ }
+
+Polygon::Polygon(const Polygon& other)
+	: ClosedPolygonalChain(other)
+{ }
+
+float Polygon::area() const {
+	int area_ = 0;
+
+	for (int i = 0; i != PointNum - 1; i++) {
+		area_ += AllPoints[i].getX() * AllPoints[i + 1].getY();
+	}
+	area_ += AllPoints[PointNum - 1].getX() * AllPoints[0].getY();
+	for (int i = 0; i != PointNum - 1; i++) {
+		area_ -= AllPoints[i + 1].getX() * AllPoints[i].getY();
+	}
+	area_ -= AllPoints[0].getX() * AllPoints[PointNum - 1].getY();
+
+	return (float)abs(area_) / 2;
+}
+
+Triangle::Triangle(int n, Point* a) 
+	: Polygon(n, a) 
+{ }
+
+Triangle::Triangle(const Triangle& other) 
+	: Polygon(other) 
+{ }
+
+bool Triangle::hasRightAngle() const {
+	if (((AllPoints[1].getX() - AllPoints[0].getX()) * (AllPoints[2].getX() - AllPoints[0].getX())) + ((AllPoints[1].getY() - AllPoints[0].getY()) * (AllPoints[2].getY() - AllPoints[0].getY())) == 0) {
+		return true;
+	}
+	if (((AllPoints[0].getX() - AllPoints[1].getX()) * (AllPoints[2].getX() - AllPoints[1].getX())) + ((AllPoints[0].getY() - AllPoints[1].getY()) * (AllPoints[2].getY() - AllPoints[1].getY())) == 0) {
+		return true;
+	}
+	if (((AllPoints[0].getX() - AllPoints[2].getX()) * (AllPoints[1].getX() - AllPoints[2].getX())) + ((AllPoints[0].getY() - AllPoints[2].getY()) * (AllPoints[1].getY() - AllPoints[2].getY())) == 0) {
+		return true;
+	}
+	return false;
+}
+
+Trapezoid::Trapezoid(int n, Point* a) 
+	: Polygon(n, a) 
+{ }
+
+Trapezoid::Trapezoid(const Trapezoid& other) 
+	: Polygon(other) 
+{ }
+
+float Trapezoid::height() const {
+	Point* AllVectors = new Point[PointNum];
+	for (int i = 0; i != PointNum - 1; i++) {
+		AllVectors[i] = Point((AllPoints[i + 1].getX() - AllPoints[i].getX()), (AllPoints[i + 1].getY() - AllPoints[i].getY()));
+	}
+	AllVectors[PointNum - 1] = Point((AllPoints[0].getX() - AllPoints[PointNum - 1].getX()), (AllPoints[0].getY() - AllPoints[PointNum - 1].getY()));
+
+	int i = 0;
+	int j = 0;
+	bool flag = false;
+	for (; i != PointNum; i++) {
+		for (j = i + 1; j != PointNum; j++) {
+			if ((AllVectors[i].getX() * AllVectors[j].getY()) - (AllVectors[i].getY() * AllVectors[j].getX()) == 0) {
+				flag = true;
+				break;
+			}
+		}
+		if (flag)
+			break;
+	}
+
+	return ((area() * 2) / ((float)sqrt(pow(AllVectors[i].getX(), 2) + pow(AllVectors[i].getY(), 2)) + (float)sqrt(pow(AllVectors[j].getX(), 2) + pow(AllVectors[j].getY(), 2))));
+}
+
+RegularPolygon::RegularPolygon(int n, Point* a) 
+	: Polygon(n, a) 
+{ }
+
+RegularPolygon::RegularPolygon(const RegularPolygon& other) 
+	: Polygon(other) 
+{ }
+
+float RegularPolygon::area() const {
+	return (PointNum * (pow((long long)AllPoints[1].getX() - (long long)AllPoints[0].getX(), 2) + pow((long long)AllPoints[1].getY() - (long long)AllPoints[0].getY(), 2))) / (4 * tan(PI / PointNum));
+}
+
+float RegularPolygon::perimeter() const {
+	return (float)sqrt((pow((long long)AllPoints[1].getX() - (long long)AllPoints[0].getX(), 2) + pow((long long)AllPoints[1].getY() - (long long)AllPoints[0].getY(), 2))) * PointNum;
+}
--- homework1/geometry.h
+++ homework1/geometry.h
@@ -1,404 +1,120 @@
 #pragma once
 #include <math.h>
 #include <memory.h>
-//todo cpp
+#define PI 3.14159265
+//fixed cpp
 class Point {
 public:
-	Point(int x = 0, int y = 0)
-		: x_(x), y_(y)
-	{
+	Point(int x = 0, int y = 0);
 
-	}
+	Point(const Point& other);
 
-	Point(const Point& other)
-		: x_(other.x_), y_(other.y_)
-	{
+	Point& operator=(const Point& other) = default;
 
-	}
+	//fixed = default
+	~Point() = default;
 
-	Point& operator=(const Point& other) {
-		if (&other == this)
-			return *this;
+	int getX() const;
 
-		x_ = other.x_;
-		y_ = other.y_;
-
-		return *this;
-	}
-
-	//todo = default
-	~Point() {}
-
-	int getX() const {
-		return x_;
-	}
-
-	int getY() const {
-		return y_;
-	}
+	int getY() const;
 
 private:
 	int x_;
 	int y_;
 };
 
-//todo make inherit from point
-class Vector {
+//fixed make inherit from point
+class PolygonalChain {
 public:
-	Vector(int x = 0, int y = 0)
-		: x_(x), y_(y)
-	{
+	PolygonalChain(int n, Point* a);
 
-	}
+	PolygonalChain(const PolygonalChain& other);
 
-	Vector(const Vector& other)
-		: x_(other.x_), y_(other.y_)
-	{
+	PolygonalChain& operator=(const PolygonalChain& other) = default;
 
-	}
+	virtual ~PolygonalChain() = default;
 
-	Vector& operator=(const Vector& other) {
-		if (&other == this)
-			return *this;
+	Point getPoint(int i) const;
 
-		x_ = other.x_;
-		y_ = other.y_;
+	float virtual perimeter() const;
 
-		return *this;
-	}
-
-	~Vector() {}
-
-	int getX() const {
-		return x_;
-	}
-
-	int getY() const {
-		return y_;
-	}
-
-private:
-	int x_;
-	int y_;
-};
-
-
-//todo inherit from ClosedPolygonalChain
-class Polygon {
-public:
-	Polygon() = delete;
-
-	explicit Polygon(int n, Point* a)
-		: PointNum(n)
-	{
-		AllPoints = new Point[n];
-		memcpy(AllPoints, a, PointNum * sizeof(Point));
-		FindArea();
-		FindVectors();
-		FindPerimeter();
-	}
-
-	Polygon(const Polygon& other)
-		: PointNum(other.PointNum), AllPoints(other.AllPoints), AllVectors(other.AllVectors), area_(other.area_), perimeter_(other.perimeter_)
-	{
-
-	}
-
-	Polygon& operator=(const Polygon& other) {
-		if (&other == this)
-			return *this;
-
-		PointNum = other.PointNum;
-		AllPoints = other.AllPoints;
-		AllVectors = other.AllVectors;
-		area_ = other.area_;
-		perimeter_ = other.perimeter_;
-
-		return *this;
-	}
-
-	~Polygon() {}
-
-	Point getPoint(int i) const {
-		return AllPoints[i];
-	}
-
-	float area() const {
-		return area_;
-	}
-
-	int perimeter() const {
-		return perimeter_;
-	}
+	int getN() const;
 
 protected:
-	void FindArea() {
-	//todo without double until end
-		for (int i = 0; i != PointNum - 1; i++) {
-			area_ += AllPoints[i].getX() * AllPoints[i + 1].getY();
-		}
-		area_ += AllPoints[PointNum - 1].getX() * AllPoints[0].getY();
-		for (int i = 0; i != PointNum - 1; i++) {
-			area_ -= AllPoints[i + 1].getX() * AllPoints[i].getY();
-		}
-		area_ -= AllPoints[0].getX() * AllPoints[PointNum - 1].getY();
-		area_ = 0.5f * abs(area_);
-	}
-
-	void FindVectors() {
-		AllVectors = new Vector[PointNum];
-		for (int i = 0; i != PointNum - 1; i++) {
-			AllVectors[i] = Vector((AllPoints[i + 1].getX() - AllPoints[i].getX()), (AllPoints[i + 1].getY() - AllPoints[i].getY()));
-		}
-		AllVectors[PointNum - 1] = Vector((AllPoints[0].getX() - AllPoints[PointNum - 1].getX()), (AllPoints[0].getY() - AllPoints[PointNum - 1].getY()));
-	}
-
-	void FindPerimeter() {
-		for (int i = 0; i != PointNum; i++) {
-			perimeter_ += (int)sqrt(pow(AllVectors[i].getX(), 2) + pow(AllVectors[i].getY(), 2));
-		}
-	}
-
 	int PointNum;
 	Point* AllPoints;
-	Vector* AllVectors;
-	float area_;
-	//todo int perimeter is weird
-	int perimeter_;
+	//fixed copy-paste
+};
+
+class ClosedPolygonalChain : public PolygonalChain {
+public:
+	ClosedPolygonalChain(int n, Point* a);
+
+	ClosedPolygonalChain(const ClosedPolygonalChain& other);
+
+	ClosedPolygonalChain& operator=(const ClosedPolygonalChain& other) = default;
+
+	~ClosedPolygonalChain() = default;
+
+	float perimeter() const override;
+};
+
+//fixed inherit from ClosedPolygonalChain
+class Polygon : public ClosedPolygonalChain {
+public:
+	Polygon(int n, Point* a);
+
+	Polygon(const Polygon& other);
+
+	Polygon& operator=(const Polygon& other) = default;
+
+	~Polygon() = default;
+
+	float virtual area() const;
+	//fixed without double until end
+
+	//fixed int perimeter is weird
 };
 
 class Triangle : public Polygon {
 public:
-	Triangle(int n, Point* a) : Polygon(n, a) { }
+	Triangle(int n, Point* a);
 
-	Triangle(const Triangle& other) : Polygon(other) { }
+	Triangle(const Triangle& other);
 
-	Triangle& operator=(const Triangle& other) {
-		if (&other == this)
-			return *this;
+	Triangle& operator=(const Triangle& other) = default;
 
-		PointNum = other.PointNum;
-		AllPoints = other.AllPoints;
-		AllVectors = other.AllVectors;
-		area_ = other.area_;
-		perimeter_ = other.perimeter_;
+	~Triangle() = default;
 
-		return *this;
-	}
-
-	~Triangle() {}
-
-	bool hasRightAngle() const {
-		//todo u dont need flag
-		bool flag = false;
-		
-		if (((AllPoints[1].getX() - AllPoints[0].getX()) * (AllPoints[2].getX() - AllPoints[0].getX())) + ((AllPoints[1].getY() - AllPoints[0].getY()) * (AllPoints[2].getY() - AllPoints[0].getY())) == 0) {
-			flag = true;
-			return flag;
-		}
-		if (((AllPoints[0].getX() - AllPoints[1].getX()) * (AllPoints[2].getX() - AllPoints[1].getX())) + ((AllPoints[0].getY() - AllPoints[1].getY()) * (AllPoints[2].getY() - AllPoints[1].getY())) == 0) {
-			flag = true;
-			return flag;
-		}
-		if (((AllPoints[0].getX() - AllPoints[2].getX()) * (AllPoints[1].getX() - AllPoints[2].getX())) + ((AllPoints[0].getY() - AllPoints[2].getY()) * (AllPoints[1].getY() - AllPoints[2].getY())) == 0) {
-			flag = true;
-			return flag;
-		}
-		return flag;
-	}
+	//fixed u dont need flag
+	bool hasRightAngle() const;
 };
 
 class Trapezoid : public Polygon {
 public:
-	Trapezoid(int n, Point* a) : Polygon(n, a) {
-		FindHeight();
-	}
+	Trapezoid(int n, Point* a);
 
-	Trapezoid(const Trapezoid& other) : Polygon(other) {
-		height_ = other.height_;
-	}
-	//todo same arrays between objects
-	Trapezoid& operator=(const Trapezoid& other) {
-		if (&other == this)
-			return *this;
+	Trapezoid(const Trapezoid& other);
+	//fixed same arrays between objects
+	Trapezoid& operator=(const Trapezoid& other) = default;
 
-		PointNum = other.PointNum;
-		AllPoints = other.AllPoints;
-		AllVectors = other.AllVectors;
-		area_ = other.area_;
-		perimeter_ = other.perimeter_;
-		height_ = other.height_;
+	~Trapezoid() = default;
 
-		return *this;
-	}
-
-	~Trapezoid() {}
-
-	float height() const {
-		return height_;
-	}
-
-private:
-	void FindHeight() {
-		int i = 0;
-		int j = 0;
-		bool flag = false;
-		for ( ; i != PointNum; i++) {
-			for (j = i + 1; j != PointNum; j++) {
-				if ((AllVectors[i].getX() * AllVectors[j].getY()) - (AllVectors[i].getY() * AllVectors[j].getX()) == 0) {
-					flag = true;
-					break;
-				}
-			}
-			if (flag)
-				break;
-		}
-
-		height_ = ((long long)area_ * 2) / (sqrt(pow(AllVectors[i].getX(), 2) + pow(AllVectors[i].getY(), 2)) + sqrt(pow(AllVectors[j].getX(), 2) + pow(AllVectors[j].getY(), 2)));
-	}
-
-	float height_ = 0;
+	float height() const;
 };
 
-
-//todo area and perimeter
+//fixed area and perimeter
 class RegularPolygon : public Polygon {
 public:
-	RegularPolygon(int n, Point* a) : Polygon(n, a) { }
+	RegularPolygon(int n, Point* a);
 
-	RegularPolygon(const RegularPolygon& other) : Polygon(other) { }
+	RegularPolygon(const RegularPolygon& other);
 
-	RegularPolygon& operator=(const RegularPolygon& other) {
-		if (&other == this)
-			return *this;
+	RegularPolygon& operator=(const RegularPolygon& other) = default;
 
-		PointNum = other.PointNum;
-		AllPoints = other.AllPoints;
-		AllVectors = other.AllVectors;
-		area_ = other.area_;
-		perimeter_ = other.perimeter_;
+	~RegularPolygon() = default;
 
-		return *this;
-	}
+	float area() const override;
 
-	~RegularPolygon() {}
-};
-
-class PolygonalChain {
-public:
-	PolygonalChain(int n, Point* a)
-		: PointNum(n)
-	{
-		AllPoints = new Point[n];
-		memcpy(AllPoints, a, PointNum * sizeof(Point));
-		FindVectors();
-		FindPerimeter();
-	}
-
-	PolygonalChain(const PolygonalChain& other)
-		: PointNum(other.PointNum), AllPoints(other.AllPoints), AllVectors(other.AllVectors), perimeter_(other.perimeter_)
-	{
-
-	}
-
-	PolygonalChain& operator=(const PolygonalChain& other) {
-		if (&other == this)
-			return *this;
-
-		PointNum = other.PointNum;
-		AllPoints = other.AllPoints;
-		AllVectors = other.AllVectors;
-		perimeter_ = other.perimeter_;
-
-		return *this;
-	}
-
-	~PolygonalChain() {}
-
-	Point getPoint(int i) const {
-		return AllPoints[i];
-	}
-
-	int perimeter() const {
-		return perimeter_;
-	}
-
-	int getN() const {
-		return PointNum;
-	}
-
-protected:
-	//todo copy-paste
-	void FindVectors() {
-		AllVectors = new Vector[PointNum - 1];
-		for (int i = 0; i != PointNum - 1; i++) {
-			AllVectors[i] = Vector((AllPoints[i + 1].getX() - AllPoints[i].getX()), (AllPoints[i + 1].getY() - AllPoints[i].getY()));
-		}
-	}
-
-	void FindPerimeter() {
-		for (int i = 0; i != PointNum - 1; i++) {
-			perimeter_ += (int)sqrt(pow(AllVectors[i].getX(), 2) + pow(AllVectors[i].getY(), 2));
-		}
-	}
-
-	int PointNum;
-	Point* AllPoints;
-	Vector* AllVectors;
-	int perimeter_;
-};
-
-class ClosedPolygonalChain: public PolygonalChain {
-public:
-	ClosedPolygonalChain(int n, Point* a)
-		: PolygonalChain(n, a)
-	{
-		FindVectors();
-		FindPerimeter();
-	}
-
-	ClosedPolygonalChain(const ClosedPolygonalChain& other) : PolygonalChain(other) { }
-
-	ClosedPolygonalChain& operator=(const ClosedPolygonalChain& other) {
-		if (&other == this)
-			return *this;
-
-		PointNum = other.PointNum;
-		AllPoints = other.AllPoints;
-		AllVectors = other.AllVectors;
-		perimeter_ = other.perimeter_;
-
-		return *this;
-	}
-
-	~ClosedPolygonalChain() {}
-
-	Point getPoint(int i) const {
-		return AllPoints[i];
-	}
-
-	int perimeter() const {
-		return perimeter_;
-	}
-
-	int getN() const {
-		return PointNum;
-	}
-
-private:
-	void FindVectors() {
-		AllVectors = new Vector[PointNum];
-
-		for (int i = 0; i != PointNum - 1; i++) {
-			AllVectors[i] = Vector((AllPoints[i + 1].getX() - AllPoints[i].getX()), (AllPoints[i + 1].getY() - AllPoints[i].getY()));
-		}
-		AllVectors[PointNum - 1] = Vector((AllPoints[0].getX() - AllPoints[PointNum - 1].getX()), (AllPoints[0].getY() - AllPoints[PointNum - 1].getY()));
-	}
-
-	void FindPerimeter() {
-		perimeter_ = 0;
-		for (int i = 0; i != PointNum; i++) {
-			perimeter_ += (int)sqrt(pow(AllVectors[i].getX(), 2) + pow(AllVectors[i].getY(), 2));
-		}
-	}
+	float perimeter() const override;
 };
